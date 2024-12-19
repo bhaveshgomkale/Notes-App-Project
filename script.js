b2741f4dcb2e9c0d0dbcf8c46d08d143ebea1679
BHAVESH GOMKALE <143423665+bhaveshgomkale@users.noreply.github.com>
2024-12-19 13:33:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const notescontainer=document.querySelector(".notes-container");
+const createBtn=document.querySelector(".btn");
+let notes=document.querySelectorAll(".input-box");
+
+function showNotes()
+{
+    notes-Container.innerHTML==localStorage.getItems("notes");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItems("notes",notesContainer.innerHTML);
+
+}
+
+createBtn.addEventListener("click",()=>{
+    let inputBox=document.createElement("P");
+    let img=document.createElement("img");
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src="images/delete.png";
+    notescontainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName ==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName ==="P"){
+        notes=document.querySelector(".input-box");
+        notes.forEach(nt=>{
+            nt.onkeyup=function()
+            {
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown",event=>{
+    if(event.key=== "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
